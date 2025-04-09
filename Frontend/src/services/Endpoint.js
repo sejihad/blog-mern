9d3fc289a4e399ef036bb53b86a6d4dea9085ae0
unknown <ijihad639@gmail.com>
2025-04-09 23:29:44 +0600

    blog

--- Frontend/src/services/Endpoint.js
+++ Frontend/src/services/Endpoint.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const BaseUrl = "http://localhost:5000";
+export const BaseUrl = "https://blog-mern-api-zeta.vercel.app";
 const instance = axios.create({
   baseURL: BaseUrl,
 
